c7690a2196d1b462d7d0b801ca5c8b4684613ea1
agosnd <distasioagostina@gmail.com>
2023-12-17 18:49:18 -0300

    pagina de opciones funcional

--- bloqueo.js
+++ bloqueo.js
@@ -1,26 +1,21 @@
-var paginasBloqueadas = [
-    "twitter.com",
-    "x.com",
-    "tiktok.com"
-];
 
 chrome.storage.local.get("estado").then((result) => {
     if(result["estado"] == "activo")
     {
         console.log(window.location.hostname);
-        if(!document.body.innerHTML.includes('deja de boludear pelotuda'))
-        {
-            for(i=0;i<paginasBloqueadas.length;i++)
+        chrome.storage.sync.get("listaUrls").then((result) =>{
+            let listaUrls = result.listaUrls;
+            console.log(listaUrls);
+            for(i=0;i<listaUrls.length;i++)
             {
-                let pagina = paginasBloqueadas[i];
+                let pagina = listaUrls[i];
                 let host = window.location.hostname.toLowerCase();
                 if(pagina ==  host || "www."+ pagina == host)
                 {
-                    //document.body.innerHTML = '<h2>deja de boludear pelotuda.</h2>'
-                    console.log("nueva URL: ", chrome.runtime.getURL("trabajando.html"));
                     window.location.replace(chrome.runtime.getURL("trabajando.html"));
                 }
             }
-        }
+        })
+        
     }
 });
